7bdb39ef66fc8089e6df5ef4dde056d7f48b6131
Arun Govindan <arunmozhi.g@gmail.com>
2026-01-07 18:00:47 +1100

    Created the basic loop structure to get wager money.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿
+
+namespace SlotMachine
+{
+    class SlotMachineClass
+    {
+        static void Main(string[] args)
+        {
+            int money = 0;
+
+            while (true)
+            {
+                Console.WriteLine("Welcome to the Slot Machine Game!");
+                Console.Write("How much money would you like to wager? $");
+                string unparsedMoneyInput = Console.ReadLine();
+
+                if (int.TryParse(unparsedMoneyInput, out money))
+                {
+                    Console.WriteLine($"So you wanna bet ${money}?");
+                    Console.WriteLine("Excellent!");
+                    Console.WriteLine("Let's play!");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Error: Incorrect input for money. Please try again.");
+                    Console.WriteLine("Press any key to continue...");
+                    Console.ReadLine();
+                    Console.Clear();
+                }
+                
+            }
+        }
+    }
+}
