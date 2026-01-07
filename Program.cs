70a721fa2b19798b045ef07535adcd1c9af7aa96
Arun Govindan <arunmozhi.g@gmail.com>
2026-01-07 18:39:32 +1100

    Print rules of the game at the beginning of the program.

--- Program.cs
+++ Program.cs
@@ -1,6 +1,4 @@
-﻿
-
-namespace SlotMachine
+﻿namespace SlotMachine
 {
     class SlotMachineClass
     {
@@ -8,9 +6,48 @@ namespace SlotMachine
         {
             int money = 0;
 
+            Console.WriteLine("Welcome to the Slot Machine Game!");
             while (true)
             {
-                Console.WriteLine("Welcome to the Slot Machine Game!");
+                Console.Write("Are you playing the game for the first time?(Y/N) - ");
+                string firstTime = Console.ReadLine().ToLower();
+                string rulesOfTheGame = """
+                                        You enter an amount of money to play. Each spin is $1. 
+
+                                        This is the payout, for each horizontal, vertical and diagonal line,
+
+                                        @ @ @ - $2
+                                        % % % - $4
+                                        £ £ £ - $8
+                                        $ $ $ = $10 
+
+                                        If two or three lines of the same symbol match then single line payout is multiplied by the number of lines of match.
+
+                                        Jackpot is if all lines have the same symbol,
+                                        For Jackpot it is $100.
+                                        
+                                        """;
+                if (firstTime == "y")
+                {
+                    Console.Write(rulesOfTheGame);
+                    break;
+                }
+                else if (firstTime == "n")
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Error: Incorrect input. Please enter Y or y for Yes and N or n for No.");
+                    Console.WriteLine("Press any key to continue...");
+                    Console.ReadLine();
+                    Console.Clear();
+                }
+            }
+            
+
+            while (true)
+            {
                 Console.Write("How much money would you like to wager? $");
                 string unparsedMoneyInput = Console.ReadLine();
 
@@ -28,7 +65,6 @@ namespace SlotMachine
                     Console.ReadLine();
                     Console.Clear();
                 }
-                
             }
         }
     }
